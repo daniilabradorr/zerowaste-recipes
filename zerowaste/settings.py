35533f41cf99fa5eeb829703d4c58ef18171e523
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-07-07 14:19:52 +0200

    settgins6

--- zerowaste/settings.py
+++ zerowaste/settings.py
@@ -14,9 +14,9 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*","zerowaste-recipes.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["zerowaste-recipes.onrender.com",]
 
 
 # Application definition
