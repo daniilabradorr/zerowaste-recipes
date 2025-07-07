2e4ff17e470300edd3862684e56da9d57208feed
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-07-07 12:53:33 +0200

    intendo correguir erro rstacofileas

--- zerowaste/settings.py
+++ zerowaste/settings.py
@@ -136,9 +136,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 #Para producción en render usare collectstatic
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 
 MEDIA_URL = "/media/"
