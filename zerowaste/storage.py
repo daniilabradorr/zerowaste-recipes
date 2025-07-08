2675e2152c5a22e47ef0b1f1ee5c1f70bd1645cf
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-07-08 19:55:59 +0200

    intento 6

--- zerowaste/storage.py
+++
@@ -1,5 +0,0 @@
-from whitenoise.storage import CompressedManifestStaticFilesStorage
-
-class NonStrictStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    # evitar excepción en manifest
-    manifest_strict = False
