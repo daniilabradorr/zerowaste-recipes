f6ce9af97a7d30ee4ff4c03d6e68a9f641e3058c
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-07-08 20:11:05 +0200

    lo dejo con ddebug true

--- zerowaste/settings.py
+++ zerowaste/settings.py
@@ -13,7 +13,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # Lo convierto en booleano
+DEBUG = True
 
 ALLOWED_HOSTS = ["zerowaste-recipes.onrender.com"]
 
@@ -120,7 +120,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = "/media/"
