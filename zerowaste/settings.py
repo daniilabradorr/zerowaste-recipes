15fab8cb78558537df224bcd26ead1285b50bd8a
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-07-09 19:19:31 +0200

    primer intento de deploy con debug en false

--- zerowaste/settings.py
+++ zerowaste/settings.py
@@ -13,9 +13,9 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*","zerowaste-recipes.onrender.com"]
+ALLOWED_HOSTS = ["zerowaste-recipes.onrender.com"]
 
 
 # Application definition
